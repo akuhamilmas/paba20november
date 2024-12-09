a609ed42b8511e7e497004954e2fef8cab6a8c32
akuhamilmas <151131300+akuhamilmas@users.noreply.github.com>
2024-12-09 23:07:31 +0700

    asd

---
+++ app/src/main/java/com/example/latihan20november/Task.kt
@@ -0,0 +1,14 @@
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+
+@Parcelize
+data class Task(
+    val name: String,
+    val date: String,
+    val category: String,
+    val description: String,
+    var isInProgress: Boolean = false, // false: belum dikerjakan, true: sedang dikerjakan
+    var isCompleted: Boolean = false // false: belum selesai, true: sudah selesai
+) : Parcelable {
+}
